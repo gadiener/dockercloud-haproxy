380ee9c652b0478e39acf756f6ab4fbf36a9075f
Gabriele Diener <gabriele.diener@caffeina.com>
2017-08-25 15:51:16 +0200

    Moved shared functions

--- haproxy/helper/compose_mode_link_helper.py
+++ haproxy/helper/compose_mode_link_helper.py
@@ -1,4 +1,5 @@
 import logging
+import compose_mode_helper as ComposeModeHelper
 
 logger = logging.getLogger("haproxy")
 
@@ -12,7 +13,7 @@ def get_compose_mode_links(docker, haproxy_container):
     networks = haproxy_container.get("NetworkSettings", {}).get("Networks", {})
     linked_compose_services = _get_linked_compose_services(networks, project)
 
-    links = _calc_links(docker, linked_compose_services, project)
+    links = ComposeModeHelper._calc_links(docker, linked_compose_services, project)
     return links, set(["%s_%s" % (project, service) for service in linked_compose_services])
 
 
@@ -24,75 +25,13 @@ def get_additional_links(docker, additional_services):
         if len(terms) == 2:
             project = terms[0]
             service = terms[1]
-            link = _calc_links(docker, [service], project)
+            link = ComposeModeHelper._calc_links(docker, [service], project)
             if link:
                 links.update(link)
                 services.add("%s_%s" % (project, service))
             else:
                 logger.info("Cannot find the additional service: %s" % additional_service.strip())
     return links, services
-
-
-def _find_container_networks_ids(container, networks_data):
-    ids = []
-    for network in networks_data:
-        if container['Id'] in network['Containers'].keys():
-            ids.append(network['Id'])
-    return ids
-
-
-def _calc_links(docker, linked_compose_services, project):
-    links = {}
-    for _container in docker.containers():
-        container_id = _container.get("Id", "")
-        container = docker.inspect_container(container_id)
-        compose_labels = container.get("Config", {}).get("Labels", {})
-        compose_project = compose_labels.get("com.docker.compose.project", "")
-        compose_service = compose_labels.get("com.docker.compose.service", "")
-
-        if compose_project == project and compose_service in linked_compose_services:
-            service_name = "%s_%s" % (compose_project, compose_service)
-            container_name = container.get("Name").lstrip("/")
-            container_evvvars = get_container_envvars(container)
-            endpoints = get_container_endpoints(container, container_name)
-            links[container_id] = {"service_name": service_name,
-                                   "container_envvars": container_evvvars,
-                                   "container_name": container_name,
-                                   "endpoints": endpoints,
-                                   "compose_service": compose_service,
-                                   "compose_project": compose_project}
-    return links
-
-
-def get_container_endpoints(container, container_name):
-    endpoints = {}
-    container_endpoints = container.get("Config", {}).get("ExposedPorts", {})
-    for k, v in container_endpoints.iteritems():
-        if k:
-            terms = k.split("/", 1)
-            port = terms[0]
-            if len(terms) == 2:
-                protocol = terms[1]
-            else:
-                protocol = "tcp"
-            if not v:
-                v = "%s://%s:%s" % (protocol, container_name, port)
-            endpoints[k] = v
-    return endpoints
-
-
-def get_container_envvars(container):
-    container_evvvars = []
-    envvars = container.get("Config", {}).get("Env", [])
-    for envvar in envvars:
-        terms = envvar.split("=", 1)
-        container_evvvar = {"key": terms[0]}
-        if len(terms) == 2:
-            container_evvvar["value"] = terms[1]
-        else:
-            container_evvvar["value"] = ""
-        container_evvvars.append(container_evvvar)
-    return container_evvvars
 
 
 def _get_linked_compose_services(networks, project):
@@ -115,11 +54,3 @@ def _get_linked_compose_services(networks, project):
             if linked_service not in linked_services:
                 linked_services.append(linked_service)
     return linked_services
-
-
-def get_service_links_str(links):
-    return sorted(set([link.get("service_name") for link in links.itervalues()]))
-
-
-def get_container_links_str(haproxy_links):
-    return sorted(set([link.get("container_name") for link in haproxy_links.itervalues()]))
